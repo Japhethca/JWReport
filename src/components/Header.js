9535d00a47cd02b69770affc1185055fa0e33a90
japhethca <chidiebere.anyigor@gmail.com>
2020-04-04 19:08:35 +0100

    remove commented styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,6 @@ export default function AppHeader({ showHideMenu = ()=>{}, backgroundColor }) {
         <Header 
             backgroundColor={backgroundColor}
             statusBarProps={{
-                // barStyle: 'default',
-                // hidden: true,
                 backgroundColor,
             }}
             containerStyle={{
